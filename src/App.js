2ffa6df86f87fd0b375eaf6ab8b81851adddffb7
Luano Barros <luanocesar@gmail.com>
2023-03-01 21:32:21 -0400

    FEAT: Added new grid from MUI

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import './App.css';
 import NavBar from './Components/NavBar';
 import AppBar from './Components/AppBar';
 import VerticalMenu from './Components/VerticalMenu'
+import StartEditButtonGrid from './Components/StartEditButtonGrid';
 
 function App() {
+  const menu_content_style = {
+    'display' : 'flex',
+    'flex-wrap':'nowrap',
+    'width' : '100%'
+  }
+
+  const getFullStyle = {
+    'width' : '92%'
+  }
+
   return (
     <>
-      <AppBar title="AppBar"></AppBar>
-      <NavBar></NavBar>
-      <VerticalMenu></VerticalMenu>
+    <div id="AppBar"><AppBar title="AppBar"></AppBar></div>
+    <div id="NavBar"><NavBar></NavBar></div>
+    <div style={menu_content_style}>
+      <div id="Menu"><VerticalMenu></VerticalMenu></div>
+      <div id="content" style={getFullStyle}><StartEditButtonGrid></StartEditButtonGrid></div>
+    </div>
     </>
   );
 }
